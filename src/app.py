a79a388dfddd9daf2120e8b472acfb457921ea6f
Luiz Tokuhara <tokuharaluiz@gmail.com>
2025-06-20 12:00:49 -0300

    fix: app and prompt

--- src/app.py
+++ src/app.py
@@ -1,50 +1,57 @@
+from typing import Any, Generator
 import uuid
 import time
+import os
 import streamlit as st
 from dotenv import load_dotenv
 from agents.agent import Agent
 
-load_dotenv()
-
-st.set_page_config(page_title="Assistente de Investimentos em Criptoativos",
-                   page_icon=":money_with_wings:")
-st.header("Assistente de Investimentos em Criptoativos")
-with st.expander("⚠️  **Aviso Importante:**"):
-  st.write(
-    """
-    Este assistente de investimentos é um **estudo de caso e uma demonstração tecnológica**.
-    As informações e sugestões fornecidas são geradas por IA e **não devem ser consideradas como aconselhamento
-    financeiro ou recomendações de investimento**.
-    """
-  )
-
-if "session_id" not in st.session_state:
-  st.session_state.session_id = uuid.uuid4()
-if "messages" not in st.session_state:
-  st.session_state.messages = []
-if "agent" not in st.session_state:
-  st.session_state.agent = Agent()
-
-agent = st.session_state.agent
-
-def simulate_stream_data(data: str):
+def simulate_stream_data(data: str) -> Generator[str, Any, None]:
   """Simula o streaming de dados para o chat"""
   for word in data.split(" "):
     yield word + " "
     time.sleep(0.02)
 
-for msg in st.session_state.messages:
-  with st.chat_message(msg["role"]):
-    st.markdown(msg["content"])
+def main() -> None:
+  st.set_page_config(page_title="Assistente de Investimentos em Criptoativos",
+                    page_icon=":money_with_wings:")
+  st.header("Assistente de Investimentos em Criptoativos")
+  with st.expander("⚠️  **Aviso Importante:**"):
+    st.write(
+      """
+      Este assistente de investimentos é um **estudo de caso e uma demonstração tecnológica**.
+      As informações e sugestões fornecidas são geradas por IA e **não devem ser consideradas como aconselhamento
+      financeiro ou recomendações de investimento**.
+      """
+    )
 
-if prompt_user := st.chat_input("Digite sua mensagem..."):
-  st.session_state.messages.append({"role": "user", "content": prompt_user})
-  with st.chat_message("user"):
-    st.markdown(prompt_user)
+  if "session_id" not in st.session_state:
+    st.session_state.session_id = uuid.uuid4()
+  if "messages" not in st.session_state:
+    st.session_state.messages = []
+  if "agent" not in st.session_state:
+    st.session_state.agent = Agent(model=os.getenv("MODEL"),
+                                  api_key=os.getenv("OPENAI_API_KEY"),
+                                  temperature=float(os.getenv("TEMPERATURE")))
 
-  with st.chat_message("assistant"):
-    with st.spinner("Pensando..."):
-      answer = agent.chat(prompt_user, st.session_state.session_id)
-      st.write_stream(simulate_stream_data(answer))
+  agent = st.session_state.agent
 
-  st.session_state.messages.append({"role": "assistant", "content": answer})
+  for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+      st.markdown(msg["content"])
+
+  if prompt_user := st.chat_input("Digite sua mensagem..."):
+    st.session_state.messages.append({"role": "user", "content": prompt_user})
+    with st.chat_message("user"):
+      st.markdown(prompt_user)
+
+    with st.chat_message("assistant"):
+      with st.spinner("Pensando..."):
+        answer = agent.chat(prompt_user, st.session_state.session_id)
+        st.write_stream(simulate_stream_data(answer))
+
+    st.session_state.messages.append({"role": "assistant", "content": answer})
+
+if __name__ == "__main__":
+  load_dotenv()
+  main()
--- src/prompt/prompt.py
+++ src/prompt/prompt.py
@@ -15,18 +15,12 @@ class AgentPrompt:
     Data de hoje: {today}
 
     ### Ferramentas disponíveis:
-    - **get_crypto_price**: Consulta o valor atual de criptomoedas.
-    - **get_rates**: Consulta a taxa de juros da Selic, CDI e IPCA atuais.
-    - **get_crypto_history**: Consulta o histórico de preços de criptomoedas entre duas datas.
-    - **get_crypto_exchanges**: Consulta a lista de exchanges / corretoras que negociam criptomoedas.
+    {tools}
 
     ### Diretrizes:
     - Responda somente assuntos relacionados a finanças e investimentos.
     - Antes de responder, verifique se a pergunta não possui linguagem ofensiva ou imprópria.
     - Se a pergunta não for sobre finanças, informe que não pode ajudar com isso.
-    - Quando perguntado sobre quais ferramentas estão disponíveis ou o que você pode fazer, diga:
-      "Posso consultar o preço atual de criptomoedas, histórico de preços de criptomoedas entre duas datas
-      e taxas de juros como Selic e CDI."
 
     Sempre responda de forma clara e resumida.
     Se não souber a resposta, seja honesto e educado.
